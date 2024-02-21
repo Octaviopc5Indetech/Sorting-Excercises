bd688bdffefed6f46ee7a35863524cc54f481aa3
Octavio Perez <octavio@indetech.com.mx>
2024-02-20 20:36:35 -0600

    Creamos la funcion para bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -3,6 +3,18 @@ const readNumber = fs.readFileSync("num-aleat.txt").toString();
 const stringNumber = readNumber.split(",");
 const { performance } = require('perf_hooks');
 
+const bubble = arr => {
+    const swap = (list, a, b) => [list[a], list[b]] = [list[b], list[a]];
+  
+    for (let i = arr.length; i > 0; i--) {
+      for (let j = 0; j < i - 1; j++) {
+        if (arr[j] > arr[j + 1]) swap(arr, j, j + 1);
+      };
+    };
+  
+    return arr;
+  };
+  
 
 
 let numberArray = [];
@@ -14,7 +26,7 @@ for (let i of stringNumber) {
 }
 
 const startTime = performance.now();
-const sortedData = insertionSort(numberArray);
+const sortedData = bubble(numberArray);
 const endTime = performance.now();
 const runtime = endTime - startTime;
 
