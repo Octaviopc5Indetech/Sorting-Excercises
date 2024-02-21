318cab9cf77561987a8e90be0e2d44e8f4277353
Octavio Perez <octavio@indetech.com.mx>
2024-02-20 20:34:33 -0600

    Repetimos el llamado de la funcion y su desempeño

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,3 +2,22 @@ const fs = require("fs");
 const readNumber = fs.readFileSync("num-aleat.txt").toString();
 const stringNumber = readNumber.split(",");
 const { performance } = require('perf_hooks');
+
+
+
+let numberArray = [];
+for (let i of stringNumber) {
+  let num = Number(i);
+  if (!isNaN(num)) {
+    numberArray.push(num);
+  }
+}
+
+const startTime = performance.now();
+const sortedData = insertionSort(numberArray);
+const endTime = performance.now();
+const runtime = endTime - startTime;
+
+console.log(sortedData);
+
+console.log("Runtime:", runtime, "milliseconds");
